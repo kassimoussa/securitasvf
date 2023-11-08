2f0626ca3041df26678e1e1e714aa330d954d679
Kassim Moussa Ali <65886600+kassimoussa@users.noreply.github.com>
2023-11-08 15:10:23 +0300

    vf

--- resources/views/emails/messages.blade.php
+++ resources/views/emails/messages.blade.php
@@ -1,8 +1,8 @@
 <x-mail::message>
 
-<div class="text-center">
+{{-- <div class="text-center">
     <img src="{{ asset('images/logo_securitas.png') }}" alt="" srcset="" height="50" width="100%">
-</div>
+</div> --}}
 
 Vous avez un nouveau message de <strong> {{-- {{ $name }} --}} kassim</strong>
 
